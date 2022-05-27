b9b1c43ab223e18d335557620bcb4529e32510cc
Cyril LEMAIRE <cylemair@e1r8p12.42.fr>
2022-05-27 19:01:19 +0200

    change(register): password validation + add toster

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import * as React from 'react'
+import { ToastContainer } from 'react-toastify'
+import toast from '../components/Toast'
+import 'react-toastify/dist/ReactToastify.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnBottom={false}
+          draggable={false}
+          pauseOnVisibilityChange
+          closeOnClick
+          pauseOnHover
+      />
+    </>
+  )
 }
 
 export default MyApp
