d9a3b13b6bc0f37ba7c5c060b2e82e7732d91341
Lucine Gaveriaux <lucine@cit.io>
2022-10-17 19:52:26 +0200

    more char for short-bio

--- api/flaskr/models/user.py
+++ api/flaskr/models/user.py
@@ -44,7 +44,7 @@ class User(BaseModel):
 		"search_female": BooleanField(default=True),
 		"search_male": BooleanField(default=True),
 		"search_other": BooleanField(default=True),
-		"short_bio": CharField(max_length=280, null=True),
+		"short_bio": CharField(max_length=280, null=True, authorized_characters="^[a-zA-Z0-9_\-.#^'¨%,;:?!@ ]*$"),
 		"public_popularity": PositiveTinyIntegerField(max=100, null=True),
 	}
 
