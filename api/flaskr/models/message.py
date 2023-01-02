f32e2e0d8efc54fce23373e959999bd4831c7262
Lucine Gaveriaux <lucine@cit.io>
2023-01-02 18:19:48 +0100

    messages tested

--- api/flaskr/models/message.py
+++ api/flaskr/models/message.py
@@ -22,7 +22,7 @@ class Message(BaseModel):
         "destination_user_id": ForeignKeyField(to=User, required=True),
         "datetime": DatetimeField(),
         "read": BooleanField(required=True),
-        "content": CharField(required=True, max_length=1580)
+        "content": CharField(required=True, max_length=1580, authorized_characters="^[a-zA-Zéèêàùûô0-9_\-.#^'¨%,;:?!@ \x0d\x0a]*$")
     }
 
     @classmethod
@@ -41,12 +41,12 @@ def get_or_create_message(user_id: int):
     """
     Mark as read all the messages return by GET where the current user is the destination user.
     """
+    user_id = int(user_id)
     if request.method == "GET":
         users = [user_id, session["user_id"]]
         messages = Message.get(
             on_col=["author_user_id", "destination_user_id"],
             for_val=[users, users],
-            on_col_type="OR"
         ).fetchall()
         Message.update({"read": 1}, {"author_user_id": user_id, "destination_user_id": session["user_id"]})
         return Message.bulk_expose(messages, 200)
